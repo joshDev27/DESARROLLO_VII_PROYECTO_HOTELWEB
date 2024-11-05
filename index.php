54ef6b1c513e0a5e5f91c6d5bf6484cd8c34dab2
03gabriel12 <leandro.rodriguez@utp.ac.pa>
2024-11-05 15:53:16 -0500

    error en el login con las inclusiones de los archivos

--- index.php
+++ index.php
@@ -13,12 +13,12 @@ require BASE_PATH . 'src/function.php';
 // Include the configuration file
 
 require_once BASE_PATH . 'config.php';
-include  'C:/laragon/www/DESARROLLO_VII_PROYECTO_HOTELWEB/app/controller/user_controller/controller.php';
 
 // Include necessary files
 
 include BASE_PATH . 'database/conection/ConexionDba.php';
 
+include  'C:/laragon/www/DESARROLLO_VII_PROYECTO_HOTELWEB/app/controller/user_controller/controller.php';
 require BASE_PATH . 'views/layout.php';
 
 // Include the test file
